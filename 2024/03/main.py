d47e180e3eaaa7c87eb04f81f4847ff8fdbb3004
nathan <nathanberry97@live.com>
2024-12-04 18:28:19 +0000

    adding day three result

---
+++ 2024/03/main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3.11
+
+
+import re as regEx
+
+
+def main():
+    input = open('./input.txt', 'r').read()
+    digitsPattern = r"mul\(\d+,\d+\)"
+
+    print('Part one:', partOne(regEx.findall(digitsPattern, input)))
+    print('Part two:', partTwo(input, digitsPattern))
+
+
+def partOne(data: list[str]) -> int:
+    answer = 0
+
+    for val in data:
+        digits = val.replace('mul(', '').replace(')', '').split(',')
+        answer += int(digits[0]) * int(digits[1])
+
+    return answer
+
+
+def partTwo(data: str, digitsPattern: str) -> int:
+    enabled, valid_mul = True, []
+    dontPattern = r"don't\(\)"
+    doPattern = r"do\(\)"
+
+    sections = regEx.split(f"({doPattern}|{dontPattern})", data)
+
+    for section in sections:
+        if regEx.fullmatch(doPattern, section):
+            enabled = True
+        elif regEx.fullmatch(dontPattern, section):
+            enabled = False
+        elif enabled:
+            valid_mul.extend(regEx.findall(digitsPattern, section))
+
+    return(partOne(valid_mul))
+
+
+if '__main__' == __name__:
+    main()
