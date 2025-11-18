3f0d0ee4493bd8d0b0fc05956d8fc4b0277bb05c
Nathan <hey@nathanberry.co.uk>
2025-11-18 18:08:02 +0000

    (feat): Implement solution for day 02 year 2015

--- AoC/2015/src/day/02/index.test.ts
+++ AoC/2015/src/day/02/index.test.ts
@@ -1,10 +1,53 @@
 import { describe, it, expect } from 'vitest';
-import { solution } from './index';
+import { partOne, partTwo } from './index';
 
 describe('Day 02', () => {
-    it('works with sample input', () => {
-        const input = 'sample input';
-        expect(solution(input)).toBe(input);
+    describe('Part One', () => {
+        it('Test 1 ~ to be 58', () => {
+            // Arrange
+            const input = '2x3x4';
+
+            // Act
+            const result = partOne(input);
+
+            // Assert
+            expect(result).toBe(58);
+        });
+
+        it('Test 2 ~ to be 42', () => {
+            // Arrange
+            const input = '1x1x10';
+
+            // Act
+            const result = partOne(input);
+
+            // Assert
+            expect(result).toBe(43);
+        });
     });
+
+    describe('Part Two', () => {
+        it('Test 1 ~ to be 34', () => {
+            // Arrange
+            const input = '2x3x4';
+
+            // Act
+            const result = partTwo(input);
+
+            // Assert
+            expect(result).toBe(34);
+        });
+
+        it('Test 2 ~ to be 14', () => {
+            // Arrange
+            const input = '1x1x10';
+
+            // Act
+            const result = partTwo(input);
+
+            // Assert
+            expect(result).toBe(14);
+        });
+    })
 });
 
--- AoC/2015/src/day/02/index.ts
+++ AoC/2015/src/day/02/index.ts
@@ -1,4 +1,51 @@
+/**
+ * Solution to AoC 2015 day 02:
+ *  - https://adventofcode.com/2015/day/2
+ */
 export function solution(input: string) {
-    // TODO: implement day 02
-    return input;
+    return `\n\tPart One: ${partOne(input)}\n\tPart Two: ${partTwo(input)}`;
+}
+
+export function partOne(input: string): number {
+    return wrappingPaperAlogrithm(input, { feetOfRibbon: false });
+}
+
+export function partTwo(input: string): number {
+    return wrappingPaperAlogrithm(input, { feetOfRibbon: true });
+}
+
+function wrappingPaperAlogrithm(
+    input: string,
+    options: { feetOfRibbon: boolean }
+): number {
+    let result = 0
+    const dimensions = input.split('\n')
+
+    for (let i = 0; i < dimensions.length; i++) {
+        const dimension = dimensions[i].split('x');
+
+        if (dimension.length !== 3) continue;
+
+        const length = parseInt(dimension[0], 10);
+        const width = parseInt(dimension[1], 10);
+        const height = parseInt(dimension[2], 10);
+
+        if (options.feetOfRibbon) {
+            const sides = [length, width, height]
+            const volume = length * width * height;
+
+            const [a, b] = sides.sort((x, y) => x - y).slice(0, 2);
+            result += a * 2 + b * 2 + volume;
+        } else {
+            const side1 = length * width;
+            const side2 = width * height;
+            const side3 = height * length;
+
+            result +=
+                2 * (side1 + side2 + side3)
+                + Math.min(side1, side2, side3);
+        }
+    }
+
+    return result
 }
